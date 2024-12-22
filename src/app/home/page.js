64bbf415588b6915e86dc48f02a55b0279e1a008
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2024-12-22 11:54:27 -0800

    home page is updated

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -16,10 +16,21 @@ const HomeContent = () => {
 
   return (
     <Container
-      className="sm:px-4 lg:px-0 flex-grow  bg-background h-dvh w-full 
+      className="sm:px-4 lg:px-0 flex flex-col  bg-background h-dvh w-full 
                           rounded-t-xl -mt-2 overflow-y-scroll"
     >
-      <div className="absolute h-auto flex flex-col z-40 gap-2 w-[360px] mt-20 bg-white ml-5 sm:w-[600px] sm:ml-40 sm:mt-56">
+      <div className="font-bold font-sans sm:text-2xl p-5 sm:mt-20 flex justify-center">
+        <header>
+          <h1>
+            Azərbaycan Xalq Parlamentinə seçki həftəsi 23.12.2024 - 29.12.2024
+            tarixlərind
+          </h1>
+        </header>
+      </div>
+      <div
+        className="h-auto flex flex-col z-40 gap-2 w-[360px]  mt-20
+                 bg-white ml-5 sm:w-[600px] justify-between sm:ml-[300px] "
+      >
         <Link
           href="https://pollforall.com/p3omgynq"
           className="font-bold text-xl sm:text-2xl flex justify-center text-red-900 animate-pulse p-3"
@@ -33,7 +44,7 @@ const HomeContent = () => {
           height={400}
         />
       </div>
-      <section className="flex justify-center mt-3">
+      {/* <section className="flex justify-center mt-3">
         <Image
           alt="mmad"
           src={mammd}
@@ -41,7 +52,7 @@ const HomeContent = () => {
           width={600}
           className="rounded-lg w-full h-auto"
         />
-      </section>
+      </section> */}
       {links.name !== "" && (
         <Link
           href={links.url}
@@ -52,17 +63,17 @@ const HomeContent = () => {
           </span>
         </Link>
       )}
-      <div className="hidden sm:flex sm:flex-row p-1 gap-1">
+      {/* <div className="hidden sm:flex sm:flex-row p-1 gap-1">
         <div className="rounded-xl shadow-lg border border-gray-200 w-full h-auto">
           <AzxMemoazn />
         </div>
         <div className="rounded-xl shadow-lg border border-gray-200 w-full h-auto">
           <AxpMemoEng />
         </div>
-      </div>
-      <div className="sm:hidden">
+      </div> */}
+      {/* <div className="sm:hidden">
         {lang === "az" ? <AzxMemoazn /> : <AxpMemoEng />}
-      </div>
+      </div> */}
     </Container>
   );
 };
