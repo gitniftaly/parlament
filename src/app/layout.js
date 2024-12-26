8bceb72664c71b2a1da607ebeb3b70497808d311
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2024-12-26 11:57:18 -0800

    posthog analytics removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-// import Script from "next/script";
-// import { useState } from "react";
+import Script from "next/script";
 
 import Container from "@/components/Container";
 import { AppContextApi } from "@/contextapi/CreateContexApi";
-import { CSPostHogProvider } from "@/providers/ph-provider";
-
-import PostHogPageView from "@/components/posthog/post-hog-page-view";
-import { Suspense } from "react";
+// import { CSPostHogProvider } from "@/providers/ph-provider";
+// import PostHogPageView from "@/components/posthog/post-hog-page-view";
+// import { Suspense } from "react";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -32,32 +30,34 @@ export default function RootLayout({ children }) {
       <head>
         <link href="../../images/favicon.ico" alt="favicon" rel="icon" />
 
-        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HT6Q51ECSF"></Script>
-<Script id="google-analytics">
- {
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
- 
-   gtag('config', 'G-HT6Q51ECSF');
- }
-</Script> */}
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-F8MWKGLZ88"
+        />
+        <Script id="ga-script" strategy="lazyOnload">
+          {` window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F8MWKGLZ88',{
+              page_path: window.location.pathname,
+              });`}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-100 -mt-0 min-h-dvh`}
       >
-        <CSPostHogProvider>
-          <AppContextApi>
-            <Container className="py-1">
-              <Navbar className="w-[570px] flex bg-red-200" />
-              <Suspense fallback={null}>
+        {/* <CSPostHogProvider> */}
+        <AppContextApi>
+          <Container className="py-1">
+            <Navbar className="w-[570px] flex bg-red-200" />
+            {/* <Suspense fallback={null}>
                 <PostHogPageView />
-              </Suspense>
-              {children}
-              <Footer />
-            </Container>
-          </AppContextApi>
-        </CSPostHogProvider>
+              </Suspense> */}
+            {children}
+            <Footer />
+          </Container>
+        </AppContextApi>
+        {/* </CSPostHogProvider> */}
       </body>
     </html>
   );
