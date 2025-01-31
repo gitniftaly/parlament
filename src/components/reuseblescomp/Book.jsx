f5d630baf592dbc4b70b9764d4938efd11ad3971
gitniftaly <fngo2010@gmail.com>
2025-01-31 10:46:06 -0800

    eslint issue with Bookfixed

--- src/components/reuseblescomp/Book.jsx
+++ src/components/reuseblescomp/Book.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Image from "next/image";
 import Link from "next/link";
-import { TbHandClick } from "react-icons/tb";
 
 const Book = ({ data }) => {
   return (
