41951d1476b19827a4c0b2d86e77ee61c9a4edf1
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-04-21 04:35:01 +0400

    election video azxp added

--- src/components/home/ElectionSecond.jsx
+++ src/components/home/ElectionSecond.jsx
@@ -1,23 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Container from "../Container";
 
 const ElectionSecond = () => {
   return (
-    <div className="flex flex-col  h-full w-full items-center justify-center">
-      <Image
-        height={300}
-        width={300}
-        src="/temp/election2.jpg"
-        alt="election2"
-        className="py-4 pb-5"
-      />
-      <Link
-        href="https://pollforall.com/p34897dx"
-        className="pb-20 font-serif text-2xl text-blue-600 underline"
-      >
-        Azərbaycan Xalq Parlamentinə əlavə seçkilər.
-      </Link>
+    <div
+      className="grid grid-cols-1 sm:flex h-full w-full justify-evenly items-center 
+                      pb-10"
+    >
+      <section className="flex flex-col justify-center items-center">
+        <Link
+          href="https://www.youtube.com/watch?v=Ol3yDDpIPHg"
+          target="_blank"
+          className="pb-20 font-serif text-2xl text-blue-600 underline"
+        >
+          <Image
+            height={300}
+            width={300}
+            src="/temp/azxp.jpg"
+            alt="election2"
+            layout="responsive"
+            className="py-4 pb-5"
+          />
+          Səsvermə başladı! Saat 08:00-dən öz millət vəkillərinizi seçin!
+        </Link>
+      </section>
+      <section className="flex flex-col justify-center items-center">
+        <Image
+          height={300}
+          width={300}
+          src="/temp/election2.jpg"
+          alt="election2"
+          className="py-4 pb-5"
+        />
+        <Link
+          href="https://pollforall.com/p34897dx"
+          target="_blank"
+          className="pb-20 font-serif text-2xl text-blue-600 underline"
+        >
+          Azərbaycan Xalq Parlamentinə əlavə seçkilər.
+        </Link>
+      </section>
     </div>
   );
 };
