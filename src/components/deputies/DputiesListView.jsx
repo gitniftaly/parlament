168f94d04c769cbc4fb752ff34422c560744897d
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-05-31 09:39:41 +0400

    spelling issues has been fixed

--- src/components/deputies/DputiesListView.jsx
+++ src/components/deputies/DputiesListView.jsx
@@ -23,7 +23,8 @@ const DeputiesListView = () => {
   return (
     <Container className="h-dvh bg-background rounded-xl -mt-2 flex items-center justify-start flex-col">
       <div className="flex font-bold font-serif flex-wrap py-3 px-2">
-        Azərbaycan Xalq Parlamentinə seçilmiş {data.length} deputatın siyahısı.
+        Azərbaycan Xalq Parlamentinə seçilmiş {data.length} millət vəkilinin
+        siyahısı.
       </div>
       <div className=" bg-background">
         <span className="font-bold p-2">
