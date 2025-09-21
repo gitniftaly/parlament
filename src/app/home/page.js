147dc2e08dde34b477c397bdd7970ac40349293e
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-09-21 10:34:36 -0700

    azxp third election

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,8 @@
-import Home from "@/components/home/Home";
-
+import ElectionSecond from "@/components/home/ElectionSecond";
+// import Home from "@/components/home/Home";
+import React from "@radix-ui/react-dropdown-menu";
 const HomeContent = () => {
-  return <Home />;
+  return <ElectionSecond />;
 };
 
 export default HomeContent;
