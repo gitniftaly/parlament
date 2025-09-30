04b943e770ea93adb4783bde1b7cebafdf4897fd
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-09-29 18:13:58 -0700

    home page is updated

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,8 @@
-import ElectionSecond from "@/components/home/ElectionSecond";
-// import Home from "@/components/home/Home";
+// import ElectionSecond from "@/components/home/ElectionSecond";
+import Home from "@/components/home/Home";
 import React from "@radix-ui/react-dropdown-menu";
 const HomeContent = () => {
-  return <ElectionSecond />;
+  return <Home />;
 };
 
 export default HomeContent;
