56bb92a6474ad1909b71921f16dc1e9e8c46af22
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-10-11 09:52:27 -0700

    new azxp session link added

--- src/utils/useDebounce.js
+++ src/utils/useDebounce.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const useDebounce = (cb, delay) => {
   const [debounceValue, setDebounceValue] = useState(cb);
@@ -12,4 +13,13 @@ export const useDebounce = (cb, delay) => {
     };
   }, [cb, delay]);
   return debounceValue;
+};
+
+export const getDeviceId = () => {
+  let deviceId = localStorage.getItem("app-device-id");
+  if (!deviceId) {
+    deviceId = uuidv4();
+    localStorage.setItem("app-device-id", deviceId);
+  }
+  return deviceId;
 };
