6525ec527082d040da1bda9bb4368ad2f32ad7db
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-12-09 01:51:33 -0800

    deputies filter function is updated

--- src/contextapi/CreateContexApi.js
+++ src/contextapi/CreateContexApi.js
@@ -16,8 +16,12 @@ export const AppContextApi = ({ children }) => {
   const [visitorCount, setVisitorCount] = useState("");
   const [hide, setHide] = useState(true);
 
-  const dep2020 = deputies2024.sort((a, b) => (a.vote < b.vote ? 1 : -1));
+  // const dep2020 = deputies2024.sort((a, b) => (a.vote < b.vote ? 1 : -1));
 
+  // const dep2024 = dep2020.filter((dep) => !expiredDeputies.includes(dep.idn));
+
+  const safeDeputies2024 = Array.isArray(deputies2024) ? deputies2024 : [];
+  const dep2020 = safeDeputies2024.sort((a, b) => (a.vote < b.vote ? 1 : -1));
   const dep2024 = dep2020.filter((dep) => !expiredDeputies.includes(dep.idn));
 
   return (
